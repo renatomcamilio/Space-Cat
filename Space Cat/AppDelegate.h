96b44ae009c78708d688f199acd9bb6d1cc43652
dannylmathews <dannyl.mathews@gmail.com>
2014-11-02 17:38:43 -0800

    Initial Commit

---
+++ Space Cat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Space Cat
+//
+//  Created by Daniel Mathews on 2014-11-02.
+//  Copyright (c) 2014 com.lighthouselabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Space Cat/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Space Cat
+//
+
+//  Copyright (c) 2014 com.lighthouselabs. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
